9a7b161931ee6c8765780066a20993f2724af1d8
fmotaf <nando.mot@gmail.com>
2025-06-04 21:23:24 -0300

    add timer for delete

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,26 +4,37 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        String filePath = "dataSample/updated_student_habits_performance.csv"; // ajuste o nome do arquivo conforme necessário
-        int maxLines = Integer.MAX_VALUE; // lê todas as linhas
+        String filePath = "dataSample/updated_student_habits_performance.csv";
+        int maxLines = Integer.MAX_VALUE;
 
         try {
-            long start = System.currentTimeMillis();
+            long startLoad = System.currentTimeMillis();
 
             List<String[]> lines = CSVReader.readCSV(filePath, maxLines);
             DoubleLinkedList list = new DoubleLinkedList();
             StudentDataLoader.loadIntoDoubleLinkedList(lines, list);
 
-            long end = System.currentTimeMillis();
+            long endLoad = System.currentTimeMillis();
 
-            list.printAll();
-
-            System.out.println("Tempo para carregar: " + (end - start) + " ms");
             int count = 0;
             for (var link : list) {
                 count++;
             }
+            System.out.println("Tempo para carregar: " + (endLoad - startLoad) + " ms");
             System.out.println("Total de registros carregados: " + count);
+
+            long startDelete = System.currentTimeMillis();
+            while (true) {
+                boolean hasElements = false;
+                for (var link : list) {
+                    hasElements = true;
+                    break;
+                }
+                if (!hasElements) break;
+                list.delete();
+            }
+            long endDelete = System.currentTimeMillis();
+            System.out.println("Tempo para deletar todos: " + (endDelete - startDelete) + " ms");
 
         } catch (Exception e) {
             e.printStackTrace();
